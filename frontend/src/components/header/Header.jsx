78d53b2ebbd23c8f818dc7747a621dd1d6659233
AzizbekEngineer <azizbekengineer@gmail.com>
2024-08-07 15:33:52 +0500

    add

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -4,7 +4,7 @@ import "./header.scss";
 
 const Header = () => {
   const [value, setValue] = useState("");
-  const { data } = useGetUsersSearchQuery({ value });
+  const { data, error } = useGetUsersSearchQuery({ value: value.trim() });
   console.log(data);
 
   return (
@@ -22,12 +22,18 @@ const Header = () => {
         </form>
         {value && (
           <div className="header__data">
-            {data?.payload?.map((el) => (
-              <div key={el._id} className="header__data-item">
-                <h3>{el?.fname}</h3>
-                <h4>@{el?.username}</h4>
+            {error ? (
+              <p className="header__not">{error.data.msg}</p>
+            ) : (
+              <div>
+                {data?.payload?.map((el) => (
+                  <div key={el._id} className="header__data-item">
+                    <h3>{el?.fname}</h3>
+                    <h4>@{el?.username}</h4>
+                  </div>
+                ))}
               </div>
-            ))}
+            )}
           </div>
         )}
       </div>
